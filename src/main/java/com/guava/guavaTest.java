908a08e55b6c7f19239802d8507a2087872b1dbb
a-cper-cpu <1611067329@qq.com>
2022-02-19 20:14:42 +0800

    init project

---
+++ src/main/java/com/guava/guavaTest.java
@@ -0,0 +1,25 @@
+package com.guava;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.*;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author a-cper-cpu
+ * @date 2022-02-02-20:57
+ */
+public class guavaTest {
+    public static void main(String[] args) {
+        HashSet<Object> objects = Sets.newHashSet();
+        objects.add("张三");
+        objects.add("李四");
+        System.out.println(objects);
+    }
+
+
+
+}
